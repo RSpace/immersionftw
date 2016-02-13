b328068e70d08470b44836c8a727bebfcaaabac0
Casper Fabricius <me@casperfabricius.com>
2016-02-13 23:52:24 +0100

    Added basic content and some horrible styling

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+var path = require('path');
+var webpack = require('webpack');
+require('babel-polyfill');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
+var ENTRY_POINTS = {
+  index: './javascripts/index',
+};
+
+var JS_LOADERS = [
+  'babel?cacheDirectory&presets[]=react,presets[]=es2015,presets[]=stage-0'
+];
+
+var PLUGINS = [
+  new ExtractTextPlugin("[name].css")
+];
+if (IS_PRODUCTION) {
+  // Uglify in production.
+  PLUGINS.push(
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: {
+          except: ['$super', '$', 'exports', 'require']
+      },
+      sourcemap: false
+    })
+  );
+}
+
+module.exports = {
+  entry: ENTRY_POINTS,
+  output: {
+    // Bundle will be served at /bundle.js locally.
+    filename: "[name].js",
+    chunkFilename: "[id].js",
+    // Bundle will be built at ./src/media/js.
+    path: './build',
+    publicPath: '/',
+  },
+  module: {
+    loaders: [
+      {
+        // JS.
+        exclude: /(node_modules|bower_components|vr-markup)/,
+        loaders: JS_LOADERS,
+        test: /\.js$/,
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?-svgo'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style", "css", "sass")
+      },
+      { test: /\.(png|jpg)$/, loader: 'file-loader' }
+    ],
+  },
+  plugins: PLUGINS,
+  resolve: {
+    extensions: ['', '.js', '.json'],
+    fallback: path.join(__dirname, 'node_modules'),
+    modulesDirectories: [
+      'src/js',
+      'node_modules',
+    ]
+  },
+  resolveLoader: {
+    fallback: [path.join(__dirname, 'node_modules')]
+  },
+  devtool: 'source-map'
+};
