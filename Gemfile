38af7e42d295085b5c8bfb80c11e21f05ebfe487
Casper Fabricius <me@casperfabricius.com>
2016-02-16 13:19:16 +0100

    Fixed jekyll build issue and added social meta

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem 'capistrano', '~> 3.4.0'
+gem 'jekyll', '~> 3.1.1'
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,4 +2,4 @@
 lock '3.4.0'
 
 set :application, 'immersionftw'
-set :repo_url, 'git@example.com:RSpace/immersionftw.git'
+set :repo_url, 'git@github.com:RSpace/immersionftw.git'
